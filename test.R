ea7d8c091330087f9df9f7077fd92cc4468e5f2b
Sandager <jespersandager65@gmail.com>
2024-12-13 12:54:13 +0000

    1321

--- test.R
+++ test.R
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-install.packages("cronR")
-library(cronR)
-1
-22
-123123213213
-=======
 132
 123124
 1123
@@ -12,4 +5,4 @@ library(cronR)
 II
 fff
 123
->>>>>>> 15c5cf43ce8daa996db56f04c910bb98dfb67962
+
